8c112940625adf7605cdc506d4b77bf2150555f9
deepdesai1996 <deepdesai32@icloud.com>
2022-05-09 22:44:03 -0400

    Initial App setup
    - Removed Storyboard
    - Initialized VC in SceneDelegate
    - Renamed MainVC

---
+++ JobGet/JobGet/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobGet
+//
+//  Created by Deep Desai on 2022-05-09.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
